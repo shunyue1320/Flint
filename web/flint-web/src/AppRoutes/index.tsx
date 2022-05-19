3278d0160f3332045d2887811329449f72e77db7
shunyue1320 <tsunyue1320@gmail.com>
2022-05-19 22:10:23 +0800

    fix(web): move Suspense

--- web/flint-web/src/AppRoutes/index.tsx
+++ web/flint-web/src/AppRoutes/index.tsx
@@ -7,24 +7,16 @@ import { routePages } from "./route-pages";
 export const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        {Object.keys(routeConfig).map(((name: keyof RouteConfig) => {
-          const { path } = routeConfig[name];
-          const { component: PageComponent } = routePages[name];
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {Object.keys(routeConfig).map(((name: keyof RouteConfig) => {
+            const { path } = routeConfig[name];
+            const { component: PageComponent } = routePages[name];
 
-          return (
-            <Route
-              key={name}
-              element={
-                <Suspense fallback={<div>Loading...</div>}>
-                  <PageComponent />
-                </Suspense>
-              }
-              path={path}
-            />
-          );
-        }) as (name: string) => React.ReactElement)}
-      </Routes>
+            return <Route key={name} element={<PageComponent />} path={path} />;
+          }) as (name: string) => React.ReactElement)}
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
