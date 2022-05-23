62eb792a98493aa4a47b564fe7d12341e06818eb
shunyue1320 <tsunyue1320@gmail.com>
2022-05-23 23:09:03 +0800

    chore(web): add LoginPanel component

---
+++ web/flint-web/src/pages/LoginPage/LoginPanel.tsx
@@ -0,0 +1,15 @@
+import "./LoginPanel.less";
+
+import React from "react";
+import { Cover } from "./icons/Cover";
+
+export interface LoginPanelProps {}
+
+export const LoginPanel: React.FC<LoginPanelProps> = ({ children }) => {
+  return (
+    <div className="login-panel-container">
+      <div className="login-panel-cover">{<Cover />}</div>
+      <div className="login-panel-inner">{children}</div>
+    </div>
+  );
+};
--- web/flint-web/src/pages/LoginPage/index.tsx
+++ web/flint-web/src/pages/LoginPage/index.tsx
@@ -1,7 +1,14 @@
+import "./style.less";
+
 import React from "react";
+import { LoginPanel } from "./LoginPanel";
 
 export const LoginPage: React.FC = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="login-page-container">
+      <LoginPanel>1234</LoginPanel>
+    </div>
+  );
 };
 
 export default LoginPage;
