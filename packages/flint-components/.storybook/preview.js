de6757e79ec3c5e013905a4e7299d93ff9c35a97
shunyue1320 <tsunyue1320@gmail.com>
2022-06-22 16:58:42 +0800

    feat(flint-components): init storybook theme

--- packages/flint-components/.storybook/preview.js
+++ packages/flint-components/.storybook/preview.js
@@ -1,9 +1,13 @@
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
+    controls: {
+      expanded: true,
+      hideNoControlsWarning: true,
+    },
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
