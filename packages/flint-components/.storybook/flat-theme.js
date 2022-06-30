7e0269989c6215a2232c6a97923ba8fc6680eff2
shunyue1320 <tsunyue1320@gmail.com>
2022-06-30 21:28:39 +0800

    fix(flint-components): flint theme

--- packages/flint-components/.storybook/flat-theme.js
+++
@@ -1,9 +0,0 @@
-import { create } from "@storybook/theming";
-import brandImage from "./flat-components.png";
-
-export default create({
-    base: "light",
-    brandTitle: "Flat Components",
-    brandUrl: "https://github.com/netless-io/flat",
-    brandImage: brandImage,
-});
--- packages/flint-components/.storybook/manager.js
+++ packages/flint-components/.storybook/manager.js
@@ -1,9 +1,9 @@
 import { addons } from "@storybook/addons";
-import flatTheme from "./flat-theme";
+import flintTheme from "./flint-theme";
 import "style-loader!css-loader!less-loader!../src/theme/colors.less";
 
 document.body.classList.add("flat-colors-root");
 
 addons.setConfig({
-  theme: flatTheme,
+  theme: flintTheme,
 });
