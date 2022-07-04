b26f58818bc68836e825ee4d1467e327bfc95b84
shunyue1320 <tsunyue1320@gmail.com>
2022-07-04 22:12:40 +0800

    chore(web): add i18n

--- web/flint-web/src/tasks/init-ui.tsx
+++ web/flint-web/src/tasks/init-ui.tsx
@@ -1,19 +1,57 @@
 import "flint-components/theme/index.less";
 import "../theme.less";
 
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import ReactDOM from "react-dom/client";
 
+import { ConfigProvider } from "antd";
+import zhCN from "antd/es/locale/zh_CN";
+import enUS from "antd/es/locale/en_US";
+
+import { I18nextProvider } from "react-i18next";
+import { i18n } from "../utils/i18n";
 import { AppRoutes } from "../AppRoutes";
 import { StoreProvider } from "../components/StoreProvider";
+import { useUpdate } from "react-use";
 
 const App: React.FC = () => {
+  const forceUpdate = useUpdate();
+  const antdLocale = useMemo(
+    () => (i18n.language.startsWith("zh") ? zhCN : enUS),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [i18n.language],
+  );
+
+  useEffect(() => {
+    const onLangChanged = (): void => {
+      forceUpdate();
+    };
+
+    i18n.on("onLangChanged", onLangChanged);
+
+    return () => {
+      i18n.off("onLangChanged", onLangChanged);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <StoreProvider>
-      <AppRoutes />
-    </StoreProvider>
+    <I18nextProvider i18n={i18n}>
+      <ConfigProvider
+        autoInsertSpaceInButton={false}
+        getPopupContainer={getPopupContainer}
+        locale={antdLocale}
+      >
+        <StoreProvider>
+          <AppRoutes />
+        </StoreProvider>
+      </ConfigProvider>
+    </I18nextProvider>
   );
 };
+
+function getPopupContainer(trigger?: HTMLElement): HTMLElement {
+  return trigger?.parentElement || document.body;
+}
 
 export const initUI = (): void => {
   ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
--- web/flint-web/vite.config.ts
+++ web/flint-web/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import { dotenv } from "./scripts/vite-plugin-dotenv";
 import { autoChooseConfig } from "../../scripts/utils/auto-choose-config";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dotenv(autoChooseConfig())],
   server: {
