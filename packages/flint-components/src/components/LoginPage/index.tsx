1466f27bd94aff4e1a926c094515a08ac2a2f314
shunyue1320 <tsunyue1320@gmail.com>
2022-07-16 23:13:11 +0800

    chore(web): login page support dark mode

--- packages/flint-components/src/components/LoginPage/index.tsx
+++ packages/flint-components/src/components/LoginPage/index.tsx
@@ -1,7 +1,10 @@
 import "./index.less";
 
-import React from "react";
+import React, { useContext } from "react";
+
 import { Cover } from "./icons/Cover";
+import { CoverDark } from "./icons/CoverDark";
+import { DarkModeContext } from "../FlintThemeProvider";
 
 export * from "./LoginWithPhone";
 export interface LoginPanelProps {
@@ -9,9 +12,10 @@ export interface LoginPanelProps {
 }
 
 export const LoginPanel: React.FC<LoginPanelProps> = ({ children }) => {
+  const darkMode = useContext(DarkModeContext);
   return (
     <div className="login-panel-container">
-      <div className="login-panel-cover">{<Cover />}</div>
+      <div className="login-panel-cover">{darkMode ? <CoverDark /> : <Cover />}</div>
       <div className="login-panel-inner">{children}</div>
     </div>
   );
