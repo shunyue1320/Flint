5a9d085bb779f22ed5ac8c3fa9b5b1f3365ef11a
shunyue1320 <tsunyue1320@gmail.com>
2022-07-19 22:14:38 +0800

    chore(web): add MainPageLayout style

--- web/flint-web/src/components/MainPageLayout.tsx
+++ web/flint-web/src/components/MainPageLayout.tsx
@@ -12,7 +12,6 @@ interface MainPageLayoutProps {
 export const MainPageLayout = observer(function MainPageLayout({ children }: MainPageLayoutProps) {
   const pageStore = useContext(PageStoreContext);
   const hasHeader = pageStore.name && routePages[pageStore.name].hasHeader;
-
   return hasHeader ? (
     <MainPageLayoutHorizontalContainer
       activeKeys={pageStore.activeKeys}
