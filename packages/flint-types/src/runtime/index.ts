0b07f0d850aaaf9870626d716e50c81dbb39797a
shunyue1320 <tsunyue1320@gmail.com>
2022-07-27 21:29:19 +0800

    chore(flint-types): add flint-types

---
+++ packages/flint-types/src/runtime/index.ts
@@ -0,0 +1,14 @@
+export type Type = {
+  isDevelopment: boolean;
+  isProduction: boolean;
+  startURL: string;
+  isMac: boolean;
+  isWin: boolean;
+  staticPath: string;
+  preloadPath: string;
+  assetsPath: string;
+  appVersion: string;
+  downloadsDirectory: string;
+};
+
+export type Key = keyof Type;
