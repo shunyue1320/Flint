9d5f3154587ec834200f4e717cdb8fbb457796d3
shunyue1320 <tsunyue1320@gmail.com>
2022-08-07 23:13:50 +0800

    chore(web): add chat panel

---
+++ packages/flint-components/src/components/ChatPanel/index.tsx
@@ -0,0 +1,55 @@
+import "./style.less";
+
+import React, { useState } from "react";
+import { Tabs } from "antd";
+import { observer } from "mobx-react-lite";
+import { useTranslation } from "react-i18next";
+
+import { SVGChat, SVGUserGroup } from "../FlatIcons";
+import { ChatTabTitle, ChatTabTitleProps } from "./ChatTabTitle";
+import { ChatMessages, ChatMessagesProps } from "./ChatMessages";
+import { ChatUsers, ChatUsersProps } from "./ChatUsers";
+
+export type ChatPanelProps = ChatTabTitleProps &
+  Omit<ChatMessagesProps, "visible"> &
+  ChatUsersProps;
+
+export const ChatPanel = observer<ChatPanelProps>(function ChatPanel(props) {
+  const { t } = useTranslation();
+  const [activeTab, setActiveTab] = useState<"messages" | "users">("messages");
+
+  return (
+    <div className="chat-panel">
+      <Tabs activeKey={activeTab} tabBarGutter={0} onChange={setActiveTab as (key: string) => void}>
+        {/* 消息列表 */}
+        <Tabs.TabPane
+          key="messages"
+          tab={
+            <ChatTabTitle>
+              <SVGChat />
+              <span>{t("messages")}</span>
+            </ChatTabTitle>
+          }
+        >
+          <ChatMessages {...props} visible={activeTab === "messages"} />
+        </Tabs.TabPane>
+
+        {/* 用户列表 */}
+        <Tabs.TabPane
+          key="users"
+          tab={
+            <ChatTabTitle {...props}>
+              <SVGUserGroup />
+              <span>{t("users")}</span>
+            </ChatTabTitle>
+          }
+        >
+          <ChatUsers {...props} />
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
+  );
+});
+
+export { ChatMessage } from "./ChatMessage";
+export * from "./types";
---
+++ packages/flint-components/src/components/ChatPanel/types.ts
@@ -0,0 +1,14 @@
+export enum ChatMsgType {
+  Notice = "Notice",
+  BanText = "BanText",
+  ChannelMessage = "ChannelMessage",
+  UserGuide = "UserGuide",
+}
+
+export type ChatMsg = {
+  uuid: string;
+  type: string;
+  value: string | boolean;
+  userUUID: string;
+  timestamp: number;
+};
