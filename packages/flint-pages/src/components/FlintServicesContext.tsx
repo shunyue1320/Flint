c74bf745e8f18b7e2f01ba4c0ed476cf47d38b1c
shunyue1320 <tsunyue1320@gmail.com>
2022-09-06 22:15:59 +0800

    fix(web): flint-services

--- packages/flint-pages/src/components/FlintServicesContext.tsx
+++ packages/flint-pages/src/components/FlintServicesContext.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createContext } from "react";
-import { FlintServices } from "@netless/flat-services";
-import { useIsomorphicLayoutEffect } from "react-use";
-import { useSafePromise } from "flat-components";
+import { FlintServices } from "@netless/flint-services";
+// import { useIsomorphicLayoutEffect } from "react-use";
+// import { useSafePromise } from "flint-components";
 
 export const FlintServicesContext = createContext<FlintServices>(null as unknown as FlintServices);
 
-export const FlintServicesContextProvider: React.FC = props => (
+export const FlintServicesContextProvider: React.FC<{ children: React.ReactNode }> = props => (
   <FlintServicesContext.Provider value={FlintServices.getInstance()}>
     {props.children}
   </FlintServicesContext.Provider>
--- web/flint-web/src/tasks/init-ui.tsx
+++ web/flint-web/src/tasks/init-ui.tsx
@@ -6,10 +6,10 @@ import ReactDOM from "react-dom/client";
 
 import { useLanguage } from "@netless/flint-i18n";
 import { AntdProvider } from "flint-components";
-import { AppRoutes } from "@netless/flat-pages/src/AppRoutes";
-import { StoreProvider } from "@netless/flat-pages/src/components/StoreProvider";
+import { AppRoutes } from "@netless/flint-pages/src/AppRoutes";
+import { StoreProvider } from "@netless/flint-pages/src/components/StoreProvider";
 // import { FlatRTCContext } from "@netless/flat-pages/src/components/FlatRTCContext";
-import { FlintServicesContextProvider } from "@netless/flat-pages/src/components/FlintServicesContext";
+import { FlintServicesContextProvider } from "@netless/flint-pages/src/components/FlintServicesContext";
 // import { getFlatRTC } from "../services/flat-rtc";
 
 // 分离MboX的全局状态避免第三方模块响应式影响 https://www.mobxjs.com/configuration#isolateglobalstate-boolean
