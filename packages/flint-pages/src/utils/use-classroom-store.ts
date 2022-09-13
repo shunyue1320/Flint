af88296035f984d46ba54fb177afc1e5076b5743
shunyue1320 <tsunyue1320@gmail.com>
2022-09-13 22:23:36 +0800

    refactor(web): init use-classroom-store

---
+++ packages/flint-pages/src/utils/use-classroom-store.ts
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { ClassroomStore, ClassroomStoreConfig } from "@netless/flint-stores";
+import { RouteNameType, usePushNavigate } from "../utils/routes";
+import { errorTips, useSafePromise } from "@netless/flint-components";
+import { FlintServices } from "@netless/flint-services";
+
+export type useClassRoomStoreConfig = Omit<
+  ClassroomStoreConfig,
+  "rtc" | "rtm" | "whiteboard" | "recording"
+>;
+
+export function useClassroomStore(config: useClassRoomStoreConfig): ClassroomStore | undefined {
+  const [classroomStore, setClassroomStore] = useState<ClassroomStore>();
+  const pushNavigate = usePushNavigate();
+  const sp = useSafePromise();
+
+  const title = classroomStore?.roomInfo?.title;
+  useEffect(() => {
+    if (title) {
+      document.title = title;
+    }
+  }, [title]);
+
+  useEffect(() => {
+    const isUnmounted = false;
+    let classroomStore: ClassroomStore | undefined;
+    const flintServices = FlintServices.getInstance();
+
+    sp(
+      Promise.all([
+        flintServices.requestService("videoChat"),
+        flintServices.requestService("textChat"),
+        flintServices.requestService("whiteboard"),
+        flintServices.requestService("recording"),
+      ]),
+    ).then(([videoChat, textChat, whiteboard, recording]) => {
+      if (!isUnmounted && videoChat && textChat && whiteboard && recording) {
+        classroomStore = new ClassroomStore({
+          ...config,
+          rtc: videoChat,
+          rtm: textChat,
+          whiteboard,
+          recording,
+        });
+
+        setClassroomStore(classroomStore);
+        sp(classroomStore.init()).catch(e => {
+          errorTips(e);
+          pushNavigate(RouteNameType.HomePage);
+        });
+      }
+    });
+
+    return () => {
+      isUnmounted = true;
+      classroomStore?.destroy().catch(e => {
+        if (process.env.NODE_ENV !== "production") {
+          console.error(e);
+        }
+      });
+
+      flintServices.shutdownService("videoChat");
+      flintServices.shutdownService("textChat");
+      flintServices.shutdownService("whiteboard");
+      flintServices.shutdownService("recording");
+    };
+  }, []);
+
+  return classroomStore;
+}
