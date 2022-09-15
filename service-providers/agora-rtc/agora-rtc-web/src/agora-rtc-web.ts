c447c078a88a8b5a519b8fb128768d251c344c97
shunyue1320 <tsunyue1320@gmail.com>
2022-09-15 22:40:48 +0800

    refactor(web): init agora-rtc-web

---
+++ service-providers/agora-rtc/agora-rtc-web/src/agora-rtc-web.ts
@@ -0,0 +1,37 @@
+import AgoraRTC from "agora-rtc-sdk-ng";
+
+import { IServiceVideoChat } from "@netless/flat-services";
+
+import { AgoraRTCWebShareScreen } from "./rtc-share-screen";
+
+export class AgoraRTCWeb extends IServiceVideoChat {
+  public readonly APP_ID: string;
+  public readonly shareScreen: AgoraRTCWebShareScreen;
+
+  public constructor({ APP_ID }: AgoraRTCWebConfig) {
+    super();
+    this.APP_ID = APP_ID;
+    this.shareScreen = new AgoraRTCWebShareScreen({ APP_ID });
+    this.sideEffect.add(() => {
+      // 监听 摄像头 改变并更新id
+      AgoraRTC.onCameraChanged = deviceInfo => {
+        this.setCameraID(deviceInfo.device.deviceId);
+      };
+      // 监听 麦克风 改变并更新id
+      AgoraRTC.onMicrophoneChanged = deviceInfo => {
+        this.setMicID(deviceInfo.device.deviceId);
+      };
+      // 监听 扬声器 改变并更新id
+      AgoraRTC.onPlaybackDeviceChanged = deviceInfo => {
+        this.setSpeakerID(deviceInfo.device.deviceId);
+      };
+
+      // 卸载时销毁副作用
+      return () => {
+        AgoraRTC.onCameraChanged = undefined;
+        AgoraRTC.onMicrophoneChanged = undefined;
+        AgoraRTC.onPlaybackDeviceChanged = undefined;
+      };
+    });
+  }
+}
---
+++ service-providers/agora-rtc/agora-rtc-web/src/rtc-share-screen.ts
@@ -0,0 +1,58 @@
+import AgoraRTC from "agora-rtc-sdk-ng";
+import type { IRemoteVideoTrack, IAgoraRTCClient, ILocalVideoTrack } from "agora-rtc-sdk-ng";
+import { SideEffectManager } from "side-effect-manager";
+import { Val } from "value-enhancer";
+
+import { IServiceShareScreen } from "@netless/flat-services";
+
+export interface AgoraRTCWebShareScreenAvatarConfig {
+  APP_ID: string;
+  element?: HTMLElement | null;
+}
+
+export class AgoraRTCWebShareScreen extends IServiceShareScreen {
+  private readonly APP_ID: string;
+  private readonly _sideEffect = new SideEffectManager();
+
+  // 远程视频跟踪
+  private readonly _remoteVideoTrack$ = new Val<IRemoteVideoTrack | null>(null);
+  private readonly _el$: Val<HTMLElement | null>;
+
+  // 客户端
+  public readonly client: IAgoraRTCClient;
+  // 本地音频轨道
+  public localVideoTrack: ILocalVideoTrack | null = null;
+
+  // 远程视频轨道
+  public remoteVideoTrack: IRemoteVideoTrack | null = null;
+
+  public constructor(config: AgoraRTCWebShareScreenAvatarConfig) {
+    super();
+    this.APP_ID = config.APP_ID;
+    // 创建声网 rtc 客户端
+    this.client = AgoraRTC.createClient({ mode: "rtc", codec: "h264" });
+    this._el$ = new Val(config.element ?? null);
+
+    this._sideEffect.addDisposer(
+      this._remoteVideoTrack$.subscribe(remoteVideoTrack => {
+        // 远程视频轨道没有 替换本地视频轨道
+        if (remoteVideoTrack) {
+          if (this.remoteVideoTrack) {
+            this.remoteVideoTrack.stop();
+          }
+          this.remoteVideoTrack = remoteVideoTrack;
+          if (this._el$.value && !this.localVideoTrack) {
+            this.remoteVideoTrack.play(this._el$.value);
+          }
+          this.events.emit("remote-changed", true);
+
+          // 远程视频轨道没有 && 本地有 则关闭本地视频轨道
+        } else if (this.remoteVideoTrack) {
+          this.remoteVideoTrack.stop();
+          this.remoteVideoTrack = null;
+          this.events.emit("remote-changed", false);
+        }
+      }),
+    );
+  }
+}
