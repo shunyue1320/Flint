16420fd26311dba2cbf3fe6a6e84ace945392c2f
shunyue1320 <tsunyue1320@gmail.com>
2022-11-16 22:30:29 +0800

    chore(scripts): fix ci script

--- scripts/ci/remove-package-scripts-hooks.js
+++ scripts/ci/remove-package-scripts-hooks.js
@@ -9,11 +9,11 @@ const rootPackageJSONPath = path.resolve(__dirname, "..", "..", "package.json");
 const packageJSONContent = require(rootPackageJSONPath);
 
 for (const scriptName in packageJSONContent.scripts) {
-    if (scriptName.startsWith("pre") || scriptName.startsWith("post")) {
-        delete packageJSONContent.scripts[scriptName];
-    }
+  if (scriptName.startsWith("pre") || scriptName.startsWith("post")) {
+    delete packageJSONContent.scripts[scriptName];
+  }
 }
 
 fs.writeFileSync(rootPackageJSONPath, JSON.stringify(packageJSONContent, null, 2), {
-    encoding: "utf-8",
+  encoding: "utf-8",
 });
--- scripts/constants.js
+++ scripts/constants.js
@@ -3,6 +3,9 @@ const { join } = require("path");
 const rootPath = join(__dirname, "..");
 const configPath = join(rootPath, "config");
 
-module.exports = {
-  configPath,
-};
+const workspacePath = join(rootPath, "pnpm-workspace.yaml");
+const rootPackageJSONPath = join(rootPath, "package.json");
+
+module.exports.configPath = configPath;
+module.exports.workspacePath = workspacePath;
+module.exports.rootPackageJSONPath = rootPackageJSONPath;
